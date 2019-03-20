014c0cedd0e6b23412d6688ca7c2592f0ae6e3fc
arerart <48734194+arerart@users.noreply.github.com>
2019-03-20 16:55:13 +0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -4,32 +4,32 @@ const gridHeight = document.querySelector('#inputHeight');
 const gridWidth = document.querySelector('#inputWidth');
 const gridCanvas = document.querySelector('#pixelCanvas');
 
-function clickedBox(event) {
-  const color = colorInput.value;
-  event.target.style.backgroundColor = color;
+function clickedBox(e) {
+ // const color = colorInput.value;
+  e.target.style.backgroundColor = colorInput.value;
 }
 
 function makeGrid() {
   // Your code goes here!
   gridCanvas.innerHTML = '';
 
-  const fragment = document.createDocumentFragment();
+ // const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < gridHeight.value; i++) {
-    const tr = document.createElement('tr');
+    let tr = gridCanvas.insertRow(i);
 
     for (let z = 0; z < gridWidth.value; z++) {
-      const td = document.createElement('td');
-      tr.appendChild(td);
+      let td = tr.insertCell(z);
+    //  tr.appendChild(td);
     }
 
-    tr.addEventListener('click', clickedBox);
-    fragment.appendChild(tr);
+    tr.addEventListener("click", clickedBox);
+   // fragment.appendChild(tr);
   }
-  gridCanvas.appendChild(fragment);
+ // gridCanvas.appendChild(fragment);
 };
 
-document.querySelector('form').addEventListener('submit', function(event){
-  event.preventDefault();
+ document.querySelector('form').addEventListener('submit', function(e){
+   e.preventDefault();
   makeGrid();
 });
